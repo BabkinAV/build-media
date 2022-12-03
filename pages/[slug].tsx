d86e92ab2217dac5bca8175edc741097ac3c21c9
Andrey Babkin <babkin.andrey@gmail.com>
2022-12-03 14:39:49 +0300

    finalized single post layout

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -3,7 +3,7 @@ import { Router, useRouter } from 'next/router';
 import Card from '../components/card/card';
 import PostContent from '../components/posts/post-content';
 -PostContent;
-let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arr = [0, 1, 2, 3];
 
 const PostPage = () => {
   const router = useRouter();
