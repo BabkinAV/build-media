b911b7d2299abac317eaa591e95c0e8c541bd53c
Andrey Babkin <babkin.andrey@gmail.com>
2022-12-05 16:47:05 +0300

    working pagination

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import Layout from '../components/layout/layout';
+import '../components/pagitnation/pagination.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
