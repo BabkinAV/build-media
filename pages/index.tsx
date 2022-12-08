3fc98ee193245f0c6ad5f035d52b1e7f4884eea7
Andrey Babkin <babkin.andrey@gmail.com>
2022-12-08 19:14:05 +0300

    connected frontend to wordpress rest api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import type { NextPage } from 'next';
 
 import Card from '../components/card/card';
 import Pagination from '../components/pagination/pagination';
-let arr = [0, 1, 2, 3, 4, 5, 6,];
+// let arr = [0, 1, 2, 3, 4, 5, 6];
 
 let pageSize = 10;
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    axios
+      .get<{}[]>('http://localhost/build-media/wp-json/wp/v2/posts/')
+      .then((res) => {
+        setPosts(res.data)
+      })
+			.catch((error: string) => {
+				console.log(error)
+			})
+  }, []);
+
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [posts, setPosts] = useState<{}[]>([]);
   return (
     <div className="grid max-w-7xl grid-cols-3 gap-5">
-      {arr.map((el) => (
-        <Card key={el} />
+      {posts.map((el) => (
+        <Card key={posts.indexOf(el)} />
       ))}
       <div className="col-span-full mb-5 text-center">
         <Pagination
