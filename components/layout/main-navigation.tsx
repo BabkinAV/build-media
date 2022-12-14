85a6321cdfe4a40e50959ee7697e6e2c09b01837
Andrey Babkin <babkin.andrey@gmail.com>
2022-12-14 18:32:23 +0300

    navbar gets data from api

--- components/layout/main-navigation.tsx
+++ components/layout/main-navigation.tsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import axios from 'axios';
 import logo from '../../public/logo.png';
 import FacebookIcon from '../icons/facebook';
 import TelegramIcon from '../icons/telegram';
 import InstagramIcon from '../icons/instagram';
 import EmailIcon from '../icons/email';
 
-const items: string[] = [
-  'Main',
-  'Industry news',
-  'Digitalization',
-  'Roadmaps',
-  'Direct speech',
-  'Technology',
-  'Renovation',
-];
-
-type mainNavigationProps = {
-  menuItems?: string[];
+type Category = {
+  id: number;
+  name: string;
+  slug: string;
 };
 
-const mainNavigation: React.FC<mainNavigationProps> = ({
-  menuItems = items,
-}) => {
+// TODO: Refactor data fetching into custom hook
+
+const MainNavigation: React.FC = () => {
+  const [categoryData, setCategoryData] = useState<Category[]>([
+    { id: 0, name: 'Main', slug: 'Main' },
+  ]);
+
+  useEffect(() => {
+    axios
+      .get<Category[]>(
+        `http://localhost/build-media/wp-json/wp/v2/categories?_fields=name,%20id,%20slug`
+      )
+      .then((res) => {
+        setCategoryData([{ id: 0, name: 'Main', slug: 'Main' }, ...res.data]);
+      })
+      .catch((error: string) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <aside className="fixed h-[100%] w-[222px]  overflow-auto bg-asphalt">
       <div className="ml-8 mr-8 mt-8 flex h-[90%] flex-col">
@@ -33,13 +43,10 @@ const mainNavigation: React.FC<mainNavigationProps> = ({
         <nav>
           <div className="mr-5 mt-12">
             <ul className="font-bold text-white">
-              {menuItems.map((el) => (
-                <li
-                  key={menuItems.indexOf(el)}
-                  className="mb-[6px] first:text-orange"
-                >
-                  <Link href="#" className="hover:text-orange">
-                    {el}
+              {categoryData.map((el) => (
+                <li key={el.id} className="mb-[6px] first:text-orange">
+                  <Link href={`category/${el.slug}`} className="hover:text-orange">
+                    {el.name}
                   </Link>
                 </li>
               ))}
@@ -52,10 +59,12 @@ const mainNavigation: React.FC<mainNavigationProps> = ({
           <InstagramIcon />
           <EmailIcon />
         </div>
-				<p className='text-lightGrey text-xs mt-2'>Created by Andrey V. Babkin, 2022</p>
+        <p className="mt-2 text-xs text-lightGrey">
+          Created by Andrey V. Babkin, 2022
+        </p>
       </div>
     </aside>
   );
 };
 
-export default mainNavigation;
+export default MainNavigation;
